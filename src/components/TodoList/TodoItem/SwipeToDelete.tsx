103f57fee138d91df80ee208c5ed18bcf391d5a1
Manol Donev <manol.donev@gmail.com>
2021-11-26 18:51:49 +0200

    feat: swipe to delete todo items on mobile (#1)

---
+++ src/components/TodoList/TodoItem/SwipeToDelete.tsx
@@ -0,0 +1,135 @@
+import { useRef, useState } from 'react';
+import styled from 'styled-components/macro';
+import type {
+  SwipeCallback,
+  SwipeDirections,
+  TapCallback,
+} from 'react-swipeable';
+import { LEFT, RIGHT, useSwipeable } from 'react-swipeable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+const Wrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+`;
+
+const BackgroundLayer = styled.div<{
+  swipeDirection: SwipeDirections;
+}>`
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5rem;
+  color: white;
+  background-color: #c70000;
+
+  ${(props) =>
+    props.swipeDirection === LEFT &&
+    `
+    justify-content: flex-end;
+  `}
+
+  ${(props) =>
+    props.swipeDirection === RIGHT &&
+    `
+    justify-content: flex-start;
+  `}
+`;
+
+const ForegroundLayer = styled.div`
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+`;
+
+const SwipeToDelete = ({
+  children,
+  threshold = 0.3,
+  onSwiped,
+  onTap,
+}: {
+  children: React.ReactNode;
+  threshold?: number;
+  onSwiped?: SwipeCallback;
+  onTap?: TapCallback;
+}): JSX.Element | null => {
+  const foregroundRef = useRef<HTMLDivElement | null>(null);
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
+  const [swipeDirection, setSwipeDirection] = useState<SwipeDirections>(LEFT);
+
+  const swipeableHandlers = useSwipeable({
+    // if tracking mouse see https://github.com/FormidableLabs/react-swipeable/issues/231 for a workaround
+    trackMouse: false,
+    preventDefaultTouchmoveEvent: true,
+    onTap,
+    onSwipeStart: () => {
+      if (foregroundRef.current) {
+        foregroundRef.current.style.transition = '';
+        foregroundRef.current.style.transform = '';
+      }
+    },
+    onSwiping: (eventData) => {
+      if (foregroundRef.current) {
+        const transform = `translateX(${eventData.deltaX}px)`;
+        foregroundRef.current.style.transform = transform;
+      }
+
+      if (backgroundRef.current) {
+        const opacity = Math.min(Math.abs(eventData.deltaX) / 100, 1);
+        backgroundRef.current.style.opacity = opacity.toFixed(2);
+      }
+
+      setSwipeDirection(eventData.dir);
+    },
+    onSwiped: (eventData) => {
+      if (!foregroundRef.current) {
+        return;
+      }
+
+      let left = eventData.deltaX;
+      const { offsetWidth } = foregroundRef.current;
+      if (Math.abs(left) >= offsetWidth * threshold) {
+        left = eventData.dir === LEFT ? -offsetWidth * 2 : offsetWidth;
+
+        if (onSwiped) {
+          onSwiped(eventData);
+        }
+      } else {
+        left = 0;
+      }
+
+      foregroundRef.current.style.transition = 'transform 0.5s ease-out';
+      foregroundRef.current.style.transform = `translateX(${left}px)`;
+    },
+  });
+
+  const refPassthrough = (element: HTMLDivElement): void => {
+    swipeableHandlers.ref(element);
+    foregroundRef.current = element;
+  };
+
+  return (
+    <Wrapper>
+      <BackgroundLayer ref={backgroundRef} swipeDirection={swipeDirection}>
+        <FontAwesomeIcon icon={faTrashAlt} fixedWidth />
+      </BackgroundLayer>
+      <ForegroundLayer
+        onMouseDown={swipeableHandlers.onMouseDown}
+        ref={refPassthrough}
+      >
+        {children}
+      </ForegroundLayer>
+    </Wrapper>
+  );
+};
+
+export { SwipeToDelete };
--- src/components/TodoList/TodoItem/TodoItem.tsx
+++ src/components/TodoList/TodoItem/TodoItem.tsx
@@ -2,17 +2,21 @@ import styled from 'styled-components/macro';
 import { useQueryClient } from 'react-query';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import type { DataItem } from '../../../context/todo';
 import {
   useDeleteTodoMutation,
   useUpdateTodoMutation,
 } from '../../../generated';
+import { SwipeToDelete } from './SwipeToDelete';
 import { ImageButton } from '../../common/ImageButton';
 
 const ListItem = styled.li<{ done: boolean }>`
+  display: flex;
   background-color: #fff;
   border: none;
   border-bottom: 0.1rem solid #ddd;
+  min-height: 5rem;
 
   &:last-child {
     border-bottom: none;
@@ -33,7 +37,31 @@ const Wrapper = styled.div`
 `;
 
 const Checkbox = styled.input.attrs({ type: 'checkbox' })`
-  margin-right: 0.4rem;
+  flex: 1 0 auto;
+  margin: 0 1.5rem;
+  height: 2rem;
+  width: 2rem;
+`;
+
+const Label = styled.label`
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+`;
+
+const OverlayWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const DeleteButton = styled(ImageButton)`
+  background-color: white;
+  padding: 0 1.5rem;
+  height: 100%;
 `;
 
 const TodoItem = ({ data }: { data: DataItem }): JSX.Element => {
@@ -47,25 +75,37 @@ const TodoItem = ({ data }: { data: DataItem }): JSX.Element => {
     onSuccess: async () => queryClient.invalidateQueries('Todos'),
   });
 
-  const handleChange = (): void => {
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+  const toggleItem = (): void => {
     updateTodo({ id, input: { done: !done } });
   };
 
-  const handleClick = (): void => {
+  const deleteItem = (): void => {
     deleteTodo({ id });
   };
 
   return (
-    <ListItem done={done}>
-      <Wrapper>
-        <label style={{ marginRight: 'auto' }} htmlFor={id}>
-          <Checkbox id={id} checked={done} onChange={handleChange} />
-          {task}
-        </label>
-        <ImageButton onClick={handleClick} aria-label="Delete Item">
-          <FontAwesomeIcon icon={faTrashAlt} />
-        </ImageButton>
-      </Wrapper>
+    <ListItem
+      done={done}
+      onMouseEnter={() => setOverlayVisible(true)}
+      onMouseLeave={() => setOverlayVisible(false)}
+    >
+      <SwipeToDelete onSwiped={deleteItem} onTap={toggleItem}>
+        <Wrapper>
+          <Label htmlFor={id}>
+            <Checkbox id={id} checked={done} onChange={toggleItem} />
+            {task}
+          </Label>
+        </Wrapper>
+        {overlayVisible && (
+          <OverlayWrapper>
+            <DeleteButton onClick={deleteItem} aria-label="Delete Item">
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </DeleteButton>
+          </OverlayWrapper>
+        )}
+      </SwipeToDelete>
     </ListItem>
   );
 };
